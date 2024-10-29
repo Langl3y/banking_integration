1aeab90bce7240a4ee824bb0bd9f8e6040babe23
Langl3y <hieuhovan954@gmail.com>
2024-10-30 00:51:57 +0700

    change serialization from Pydantic to FastAPI Query (for capturing callback params)

--- routers/acb/acb_callback.py
+++ routers/acb/acb_callback.py
@@ -1,14 +1,11 @@
-from fastapi import status
+from fastapi import status, Query
 from typing import Optional
 from requests import Request
 
-from serializers.acb import AuthCallbackData
 from views import acb_router
 
 
-@acb_router.get('/authentication/callback')
-async def get_callback_code(data: AuthCallbackData):
-    auth_code = data.auth_code
-    user_id = data.user_id
-
+@acb_router.get('/authentication')
+async def get_callback_code(auth_code: str = Query(...), user_id: str = Query(...)):
+    print(auth_code)
     return {"auth_code": auth_code, "user_id": user_id}
