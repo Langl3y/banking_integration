a87823a8f148f4bddba6dcc38bc1abbc92a16546
Langl3y <hieuhovan954@gmail.com>
2024-11-06 23:50:30 +0700

    add authentication utilities

--- api/common/utils.py
+++ api/common/utils.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+import jwt
+import bcrypt
+
+from datetime import datetime, timedelta, timezone
+from typing import Union
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+import api.common.responses
 from be.env import *
+from api.common.responses import APIResponseCode
 from api.models.base_model import Base
 
 db_url = f'{db_manager}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
@@ -25,3 +34,29 @@ def create_database():
     Create all tables from defined models in api.models
     """
     Base.metadata.create_all(bind=engine)
+
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt(rounds=10)
+    hashed_password = bcrypt.hashpw(password.encode(), salt).decode()
+
+    return hashed_password
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret, algorithm=algorithm)
+    return encoded_jwt
+
+
+def decode_jwt(access_token: str):
+    try:
+        payload = jwt.decode(access_token, secret, algorithms=[algorithm])
+        return payload
+    except jwt.InvalidTokenError as e:
+        return e
